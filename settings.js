f8047289d03e1cc398a4f228943453e013b117cb
reshqamna-hue <reshqamna@gmail.com>
2025-12-27 10:59:50 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
+  packname: 'The last son😎',
+  author: 'Amour Ar‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Professor', // LAsT 
+  ownerNumber: '255715457053', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
